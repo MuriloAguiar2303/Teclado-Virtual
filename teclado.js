05f1f229748608e77285e82c7020f730502d46e6
Murilo Henrique de Aguiar <murilo.aguiar2303@gmail.com>
2023-05-22 19:13:19 -0300

    Teclado Virtual

---
+++ teclado.js
@@ -0,0 +1,8 @@
+function aperta(lt){
+    document.getElementById("aperta").innerHTML = `Você apertou a letra ${lt}`;
+}
+
+function solta(lt){
+    document.getElementById("solta").innerHTML = "Você soltou a tecla"+lt;
+}
+
